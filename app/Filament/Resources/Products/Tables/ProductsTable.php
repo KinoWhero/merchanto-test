e07321ef6a5927332bd86bfdd28da02ce564dbd4
KinoWhero <kinowhero@gmail.com>
2026-06-19 18:33:04 +0200

    fix: filament product and product categories tables

--- app/Filament/Resources/Products/Tables/ProductsTable.php
+++ app/Filament/Resources/Products/Tables/ProductsTable.php
@@ -8,6 +8,7 @@ use Filament\Actions\EditAction;
 use Filament\Actions\ForceDeleteBulkAction;
 use Filament\Actions\RestoreBulkAction;
 use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\TrashedFilter;
 use Filament\Tables\Table;
 
@@ -17,7 +18,9 @@ class ProductsTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name')->searchable(),
+                TextColumn::make('price')->money('USD'),
+                TextColumn::make('stock_quantity')->numeric(),
             ])
             ->filters([
                 TrashedFilter::make(),
