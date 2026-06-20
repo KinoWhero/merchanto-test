f67a5ca059b0e943e3008ba88184e94ec53f4ac8
KinoWhero <kinowhero@gmail.com>
2026-06-20 15:08:18 +0200

    chore: remove commented lines

--- Modules/Catalog/routes/web.php
+++ Modules/Catalog/routes/web.php
@@ -3,6 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Catalog\Http\Controllers\CatalogController;
 
-//Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('catalogs', CatalogController::class)->names('catalog');
-//});
+Route::resource('catalogs', CatalogController::class)->names('catalog');
--- Modules/Order/routes/web.php
+++ Modules/Order/routes/web.php
@@ -3,6 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Order\Http\Controllers\OrderController;
 
-//Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('orders', OrderController::class)->names('order');
-//});
+Route::resource('orders', OrderController::class)->names('order');
