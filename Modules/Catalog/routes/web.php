616354329eee17d5e816a7a6fd17c2a8a12209c9
KinoWhero <kinowhero@gmail.com>
2026-06-20 14:45:34 +0200

    fix: remove middleware group from product and order browsers

--- Modules/Catalog/routes/web.php
+++ Modules/Catalog/routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Catalog\Http\Controllers\CatalogController;
 
-Route::middleware(['auth', 'verified'])->group(function () {
+//Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('catalogs', CatalogController::class)->names('catalog');
-});
+//});
