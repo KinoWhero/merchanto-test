af8193631f7ae5e435ecb44d255fe94330c141cc
KinoWhero <kinowhero@gmail.com>
2026-06-21 13:14:48 +0200

    style: order status color for public view

--- Modules/Order/app/Enums/OrderStatus.php
+++ Modules/Order/app/Enums/OrderStatus.php
@@ -28,4 +28,14 @@ enum OrderStatus: string
             self::Delivered => 'success',
         };
     }
+
+    public function badgeClasses(): string
+    {
+        return match ($this) {
+            self::Pending => 'bg-gray-100 text-gray-800 ring-gray-500/10',
+            self::Confirmed => 'bg-yellow-100 text-yellow-800 ring-yellow-600/20',
+            self::Shipped => 'bg-blue-100 text-blue-800 ring-blue-700/10',
+            self::Delivered => 'bg-green-100 text-green-800 ring-green-600/20',
+        };
+    }
 }
