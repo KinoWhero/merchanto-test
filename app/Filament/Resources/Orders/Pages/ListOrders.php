13a287500da0dbb44cc4c06c4a2fa216e14d6c2a
KinoWhero <kinowhero@gmail.com>
2026-06-22 08:15:30 +0200

    fix(admin): remove "new order" button

--- app/Filament/Resources/Orders/Pages/ListOrders.php
+++ app/Filament/Resources/Orders/Pages/ListOrders.php
@@ -9,11 +9,4 @@ use Filament\Resources\Pages\ListRecords;
 class ListOrders extends ListRecords
 {
     protected static string $resource = OrdersResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            CreateAction::make(),
-        ];
-    }
 }
